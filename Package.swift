9c237d89a85d270f00d4888c8651a47a447c04f1
Paul Schifferer <paul@schifferers.net>
2018-08-11 15:30:56 -0700

    Initial.

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "sweetrpg-security-api",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+
+        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+
+        // 👤 Authentication and Authorization layer for Fluent.
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
+
+        // JSON-API
+//        .package(url: "https://github.com/vapor-tools/vapor-jsonapi.git", .branch("master"))
+
+        // Model
+        .package(url: "ssh://git@github.com/paulyhedral/sweetrpg-security-model.git", .branch("develop"))
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Authentication", "FluentSQLite", "Vapor", "sweetrpg-security-model"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ]
+)
---
+++ Sources/App/Controllers/TokenController.swift
@@ -0,0 +1,14 @@
+import Vapor
+import SweetRPGSecurityModel
+
+
+/// Creates new users and logs them in.
+final class TokenController : RouteCollection {
+func boot(router : Router) throws {
+
+}
+
+func loginHandler(_ req : Request) throws -> Future<Token> {
+
+}
+}
